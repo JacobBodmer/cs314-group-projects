a2324d0eff14478d4ecc9ce9376cb00c1083333c
Jeff Turley <jturley@rams.colostate.edu>
2010-10-02 11:29:56 -0600

    Creating TestRoom class

---
+++ src/cs314/a3/test/TestRoom.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package cs314.a3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cs314.a3.src.Door;
+import cs314.a3.src.Key;
+import cs314.a3.src.Player;
+import cs314.a3.src.Room;
+import cs314.a3.src.Wall;
+
+public class TestRoom {
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After public void tearDown() {
+        
+    }
+}
